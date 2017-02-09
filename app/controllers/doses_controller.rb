2d9ed174e4a19205cd79a00c9d9d1d59d8e35d1f
Benjamin Lambrou <lambroubenjamin@gmail.com>
2017-02-09 17:00:24 +0100

    correction rake

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,8 +1,5 @@
 class DosesController < ApplicationController
-  before_action :set_cocktail, only: [:new, :create, :destroy]
-  before_action :set_ingredient, only: :create
-
-
+  before_action :set_cocktail, only: [:new, :create]
   def new
     @dose = Dose.new
     @ingredients = Ingredient.all
@@ -10,7 +7,6 @@ class DosesController < ApplicationController
 
   def create
     @dose = Dose.new(dose_params)
-    @dose.ingredient = @ingredient
     @dose.cocktail = @cocktail
     if @dose.save
       redirect_to cocktail_path(@cocktail)
@@ -22,7 +18,7 @@ class DosesController < ApplicationController
   def destroy
     @dose = Dose.find(params[:id])
     @dose.destroy
-    redirect_to cocktail_path(@cocktail)
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
 
@@ -31,12 +27,10 @@ class DosesController < ApplicationController
       @cocktail = Cocktail.find(params[:cocktail_id])
     end
 
-    def set_ingredient
-      @ingredient = Ingredient.find(params[:dose][:ingredient_id])
-    end
+
 
     def dose_params
-      params.require(:dose).permit(:description)
+      params.require(:dose).permit(:description, :ingredient_id)
     end
 end
 
