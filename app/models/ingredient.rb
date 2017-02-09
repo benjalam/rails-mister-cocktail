982b654145ee08002f32fdb1432f7f3e347fd0fc
Benjamin Lambrou <lambroubenjamin@gmail.com>
2017-02-09 12:07:50 +0100

    Modification on validation

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,14 @@
 class Ingredient < ApplicationRecord
   has_many :doses
   validates :name, uniqueness: true, presence: true
+  before_destroy :check_for_cocktails
+
+ private
+
+ def check_for_orders
+   if dose.count > 0
+     errors.add_to_base("cannot delete ingredient while cocktails exist")
+     return false
+   end
+ end
 end
